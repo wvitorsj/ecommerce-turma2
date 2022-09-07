37b61b0b8a4ba6bde9cb3a96c6bcc1077b0a222f
Welton Vitor <wvitorsj08@gmail.com>
2022-09-07 13:38:58 -0300

    Primeiro commit TDD

---
+++ src/main/java/com/wvitorcorp/ecommerceaula/EcommerceAulaApplication.java
@@ -0,0 +1,13 @@
+package com.wvitorcorp.ecommerceaula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceAulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceAulaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wvitorcorp/ecommerceaula/EcommerceAulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wvitorcorp.ecommerceaula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
